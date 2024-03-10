2828516e156c9bf9dc3a3c6de3bd767e9d394a37
Denis Filatov <denis.filatov@ideus.biz>
2024-03-11 01:35:00 +0200

    feat: Track CRUD implementation

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import {v4 as uuidv4} from "uuid";
+import {TrackDto} from "./dto/track.dto";
+import {plainToClass} from "class-transformer";
 
 @Injectable()
 export class TrackService {
+  private tracks: Track[] = [
+    {
+      id: '9ffe2ff7-7d15-0000-a000-a5e398c70e11',
+      name: 'Track-1',
+      duration: 100,
+      artistId: '9ffe2ff7-0000-0000-a000-a5e398c70e11',
+      albumId: '9ffe2ff7-0000-0000-a000-a5e398c70e12',
+    },
+    {
+      id: '9ffe2ff7-7d15-0000-a000-a5e398c70e12',
+      name: 'Track-2',
+      duration: 50,
+      artistId: '9ffe2ff7-0000-0000-a000-a5e398c70e12',
+      albumId: '9ffe2ff7-0000-0000-a000-a5e398c70e12',
+    },
+    {
+      id: '9ffe2ff7-7d15-0000-a000-a5e398c70e13',
+      name: 'Track-3',
+      duration: 150,
+      artistId: '9ffe2ff7-0000-0000-a000-a5e398c70e12',
+      albumId: '9ffe2ff7-0000-0000-a000-a5e398c70e12',
+    }
+  ];
   create(createTrackDto: CreateTrackDto) {
-    return 'This action adds a new track';
+    const track: Track = {
+      id: uuidv4(),
+      name: createTrackDto.name,
+      duration: createTrackDto.duration,
+      albumId: createTrackDto.albumId,
+      artistId: createTrackDto.artistId,
+    };
+    this.tracks.push(track);
+    return this.toDto(track);
   }
 
   findAll() {
-    return `This action returns all track`;
+    return this.tracks.map(track => this.toDto(track));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} track`;
+  findOne(id: string) {
+    const track = this.tracks.find(track => track.id === id);
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+    return this.toDto(track)
   }
 
-  update(id: number, updateTrackDto: UpdateTrackDto) {
-    return `This action updates a #${id} track`;
+  update(id: string, updateTrackDto: UpdateTrackDto) {
+    const track = this.tracks.find(track => track.id === id);
+    track.name = updateTrackDto.name;
+    track.duration = updateTrackDto.duration;
+    track.albumId = updateTrackDto.albumId;
+    track.artistId = updateTrackDto.artistId;
+
+    return this.toDto(track);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} track`;
+  remove(id: string) {
+    const track = this.tracks.find(track => track.id === id);
+
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+
+    this.tracks = this.tracks.filter(track => track.id !== id);
+  }
+
+  private toDto(track: Track): TrackDto {
+    return plainToClass(TrackDto, track);
   }
 }
